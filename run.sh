bfb7ea59d2e121186bde4d63ced240b173249ed1
kert <kaidokert@gmail.com>
2018-10-18 21:59:59 -0700

    Build with ble-central

---
+++ run.sh
@@ -0,0 +1,1 @@
+ionic cordova run ios -l -c --debug --device -- --codeSignIdentity="iPhone Developer" --developmentTeam=G6Q66Q573S --provisioningProfile=50c8b38c-8d5e-4998-b655-992342fe64fa --packageType=development
